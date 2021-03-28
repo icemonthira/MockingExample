c76d613d275577da90a17c86f9c274234cfcab37
icemonthira <icemonthira@gmail.com>
2021-03-28 15:47:57 +0200

    Added method for addition, passing first part of test

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -2,20 +2,30 @@ package com.calculator;
 
 public class Calculator {
 
+    public int add(String numbers) {
+        if (numbers.equals("")){
+            return 0;
 
+        }
+        return 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
