2aff3a051a14a77c8bcab51d70a5e8e77dd831f9
icemonthira <icemonthira@gmail.com>
2021-03-28 22:14:55 +0200

    added test using mockito and test coverage in method is 100% in EmployeeManager class

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,13 +1,19 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 class EmployeeManagerTest {
 
 
     @Test
-    void payEmployees() {
+    void payEmployeesWithTestDoubleClass() {
         EmployeeRepository employeeRepository = new TestEmployeeRepository();
         BankService bankService = new TestBankService();
         EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
@@ -17,4 +23,22 @@ class EmployeeManagerTest {
         assertEquals(employeeRepository.findAll().size(),payments);
 
     }
+
+    @Test
+    void payEmployeesWithMock() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll())
+                .thenReturn(List.of(new Employee("1", 20000.0),
+                        new Employee("2", 30000.0)));
+        BankService bankService = mock(BankService.class);
+        doThrow(new RuntimeException()).when(bankService).pay(anyString(), anyDouble());
+
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        int payments = employeeManager.payEmployees();
+
+        assertEquals(0,payments);
+
+    }
+
 }
