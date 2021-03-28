1060f83199ef26fb60a9b795bd689eb019c259b3
icemonthira <icemonthira@gmail.com>
2021-03-28 17:14:15 +0200

    Added new test for step 3

--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,7 @@ class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Step 1")
     void addition() {
         int sum = calculator.add("");
         int sum2 = calculator.add("1,2");
@@ -17,7 +19,25 @@ class CalculatorTest {
         assertEquals(0, sum);
         assertEquals(3, sum2);
         assertEquals(5, sum3);
+    }
 
+    @Test
+    @DisplayName("Step 2")
+    void addingUnknownAmountOfNumbers() {
+        int sumOfMulti = calculator.add("1,2,3,4,5,6,7,8,9");
+        int answer = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+        assertEquals(answer, sumOfMulti);
 
     }
+
+    @Test
+    @DisplayName("Step 3")
+    void addingNewLinesBetweenNumbers(){
+        int sumWithNewLines = calculator.add("1\n2,3");
+
+        assertEquals(6, sumWithNewLines);
+    }
+
+
 }
