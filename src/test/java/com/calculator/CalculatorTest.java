29faf81802e03d11faa4d73a4f6b40343533421f
icemonthira <icemonthira@gmail.com>
2021-03-28 15:40:41 +0200

    Added test for addition method

--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -1,8 +1,18 @@
 package com.calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    Calculator calculator = new Calculator();
 
+    @Test
+    void addition (){
+        int sum = calculator.add("");
+
+        assertEquals(0, sum);
+
+    }
 }
